a16431dd214dbfc05a2dcd08463ce5abf35e99f3
Kevin E. Ogbonna <kevineogbonna@gmail.com>
2019-08-08 14:19:24 +0100

    add db

---
+++ database/migrations/2019_08_02_093104_create_albums_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('artist_spotify_id');
+            $table->string('spotify_album_id');
+            $table->string('name');
+            $table->integer('limit_group');
+            $table->integer('offset_group');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
